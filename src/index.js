b9c58fbc8198d3cf7ab40aac24b7a2e1b0866b38
Roman Korotaev <kro1986@ukr.net>
2021-09-11 23:40:45 +0300

    добавлена переменная для номера страницы (для пагинации)

--- src/index.js
+++ src/index.js
@@ -14,10 +14,11 @@ console.log('Test')
 
 const apikey = '868TLNpZS8ooK19OYaEFOMlNAj6HNSqy'; //ключ авторизации для бекенда
 const eventsOnOnePage = 20; // число карточек о событиях на одной странице. ВНИМАНИЕ для экрана размера Tablet должно быть 21 карточка событий
-const keyword = 'madonna' // переменная для поиска по ключевому слову 
+const keyword = 'martin' // переменная для поиска по ключевому слову 
 const country = 'US' // поиск по странам. Здесь нужно указывать код страны (перечень на сайте с API). Подвязать их к выпадающему инпуту.
-   
-fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=${eventsOnOnePage}&keyword=${keyword}&countryCode=${country}&apikey=${apikey}`)
+const page = 1; // номер страницы (для пагинации)
+
+fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=${eventsOnOnePage}&keyword=${keyword}&countryCode=${country}&page=${page}&apikey=${apikey}`)
     .then(response => response.json())
     .then(data => console.log('data._embedded.events =', data._embedded.events))
 
