be8a872fba7d9c9166320f1bf7788aa1573c11a3
Roman-Lahoda <79223492+Roman-Lahoda@users.noreply.github.com>
2021-09-11 20:19:25 +0000

    Deploying to gh-pages from @ Roman-Lahoda/team-project-js@0ea9c489ae260ebbf2643e0f33233ab25b445caa 🚀

--- src.c7c97ae5.js
+++ src.c7c97ae5.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-js/src.61f87c08.js.map
+//# sourceMappingURL=/team-project-js/src.c7c97ae5.js.map
