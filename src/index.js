0020b253d49021b04716e1ecd7a595cf8d049c43
Roman Korotaev <kro1986@ukr.net>
2021-09-12 22:39:53 +0300

    Функция для получения промиса с одним объектом события/концерта по его ID

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import refs from './js/refs'
 
 const eventService = new EventService();
 
+/*
 refs.searchForm.addEventListener('submit', onSearchForm);
 refs.loadMore.addEventListener('click', onLoadMore);
+*/
 
 
 
-    
+// Пример доступа к фотографии события/концерта c  ID = 'vvG10Zpi2x1xYd'
+eventService.fetchEventById('vvG10Zpi2x1xYd').then(data => {
+    console.log('Это ссылка на фоторграфию события с указанным ID : ', data[0].images[0].url)
+})
+
--- src/js/events-service.js
+++ src/js/events-service.js
@@ -9,11 +9,12 @@ class EventService {
         this.country = 'US';        // поиск по странам. Пока по умолчанию США. Здесь нужно указывать код страны (перечень на сайте с API). Подвязать их к выпадающему инпуту.
         this.page = 1;              // номер страницы (для пагинации)
         this.eventsOnOnePage = 20;  // число карточек о событиях на одной странице. ВНИМАНИЕ для экрана размера Tablet должно быть 21 карточка событий
-     }
+        this.eventID =''
+    }
 
+    //Эта функция возвращает промис с массивом данных (события/концерты)
     fetchEvents() {
-        console.log (this)
-
+        // console.log (this)
         return fetch(`${BASE_URL}/events.json?size=${this.eventsOnOnePage}&keyword=${this.searchQuery}&countryCode=${this.country}&page=${this.page}&apikey=${apikey}`)
         .then(response => response.json())
         .then(data => {
@@ -21,8 +22,24 @@ class EventService {
             // console.log('data._embedded.events =');
             return data._embedded.events
         })
+        .catch(error => console.log(error))
 
+    }
+    
+
+    //Функция для получения промиса с одним объектом события/концерта по его ID
+    fetchEventById(searchId) {
+        // console.log (this)
+        this.eventID = searchId;
+        return fetch(`${BASE_URL}/events.json?&id=${this.eventID}&apikey=${apikey}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log('Event searched by Id', data._embedded.events);
+            return data._embedded.events
+        })
+        .catch(error => console.log(error))
      }
+
 
     incrementPage() {
         this.page += 1;
