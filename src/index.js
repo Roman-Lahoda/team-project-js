1051b63631831907e90667a27fbbe325122312d3
Roman Korotaev <kro1986@ukr.net>
2021-09-12 10:07:54 +0300

    Написан рабочий класс для обращения на бекенд

--- src/index.js
+++ src/index.js
@@ -1,65 +1,14 @@
 import './sass/main.scss';
 import EventService from './js/events-service'
-import eventTpl from  './templates/eventTpl.hbs'
+import eventTpl from './templates/eventTpl.hbs'
+import refs from './js/refs'
 
-
-console.log('Test')
- 
-/*   868TLNpZS8ooK19OYaEFOMlNAj6HNSqy  */
-
-// Search for events sourced by Universe in the United States with keyword “devjam”
-// https://app.ticketmaster.com/discovery/v2/events.json?keyword=devjam&source=universe&countryCode=US&apikey={apikey}
-  
-// Get a list of all events in the United States
-// https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey={apikey}
-
-
-// const apikey = '868TLNpZS8ooK19OYaEFOMlNAj6HNSqy'; //ключ авторизации для бекенда
-// const eventsOnOnePage = 20; // число карточек о событиях на одной странице. ВНИМАНИЕ для экрана размера Tablet должно быть 21 карточка событий
-// let searchQuery = 'Martin' // переменная для поиска по ключевому слову 
-// const country = 'US' // поиск по странам. Здесь нужно указывать код страны (перечень на сайте с API). Подвязать их к выпадающему инпуту.
-// const page = 1; // номер страницы (для пагинации)
-
-const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    searchInput: document.querySelector('.js-search-input'),
-    searchButton: document.querySelector('.js-search-button'),
-    eventsContainer: document.querySelector('.js-events-container'),
-    loadMore: document.querySelector('.js-load-more'),
-}
 
 const eventService = new EventService();
-
 
 refs.searchForm.addEventListener('submit', onSearchForm);
 refs.loadMore.addEventListener('click', onLoadMore);
 
-function onSearchForm (e) {
-    e.preventDefault();
-  
-   
-    eventService.query = e.currentTarget.elements.query.value;
-    eventService.resetPage();
-    // eventService.fetchEvents(EventService).then(Events => console.log(Events))
-    eventService.fetchEvents(EventService).then(Events => {
-        clearEventsContainer();
-        eventsMarkUp(Events);
-        console.log(Events);
-    })
-}
 
-
-function onLoadMore() {
-    // eventService.fetchEvents(EventService).then(Events => console.log(Events))
-     eventService.fetchEvents(EventService).then(eventsMarkUp)
- }
-
-function eventsMarkUp(array) {
-    refs.eventsContainer.insertAdjacentHTML ('beforeend', eventTpl (array) )
-}
-
-function clearEventsContainer() {
-    refs.eventsContainer.innerHTML = '';
-}
 
     
--- src/js/events-service.js
+++ src/js/events-service.js
@@ -1,28 +1,27 @@
 const apikey = '868TLNpZS8ooK19OYaEFOMlNAj6HNSqy'; //ключ авторизации для бекенда
-const eventsOnOnePage = 20; // число карточек о событиях на одной странице. ВНИМАНИЕ для экрана размера Tablet должно быть 21 карточка событий
-// let searchQuery = 'Martin' // переменная для поиска по ключевому слову 
-const country = 'US' // поиск по странам. Здесь нужно указывать код страны (перечень на сайте с API). Подвязать их к выпадающему инпуту.
-const page = 1; // номер страницы (для пагинации)
- 
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2'
+  
 
 
 class EventService {
     constructor() {
-        this.searchQuery = '';
-        this.page = 1;
+        this.searchQuery = '';      //поиск по ключевому слову. Проверял на именах исполнителей: Bob, Martim, Iglesias
+        this.country = 'US';        // поиск по странам. Пока по умолчанию США. Здесь нужно указывать код страны (перечень на сайте с API). Подвязать их к выпадающему инпуту.
+        this.page = 1;              // номер страницы (для пагинации)
+        this.eventsOnOnePage = 20;  // число карточек о событиях на одной странице. ВНИМАНИЕ для экрана размера Tablet должно быть 21 карточка событий
      }
 
-    fetchEvents(EventService) {
+    fetchEvents() {
         console.log (this)
 
-    return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=${eventsOnOnePage}&keyword=${this.searchQuery}&countryCode=${country}&page=${this.page}&apikey=${apikey}`)
+        return fetch(`${BASE_URL}/events.json?size=${this.eventsOnOnePage}&keyword=${this.searchQuery}&countryCode=${this.country}&page=${this.page}&apikey=${apikey}`)
         .then(response => response.json())
         .then(data => {
             this.incrementPage();
             // console.log('data._embedded.events =');
-            
             return data._embedded.events
         })
+
      }
 
     incrementPage() {
@@ -39,8 +38,32 @@ class EventService {
 
     set query(newQuery) {
         this.searchQuery = newQuery;
-     }
+    }
+
+    get queryByCountry() {
+        return this.country;
+    }
+
+    set queryByCountry(newCountry) {
+        this.country = newCountry;
+    }
+    
+    get numberOfPage() {
+         return this.page;
+     };
+
+    set numberOfPage(newPage) {
+        this.page = newPage;
+     };
+        
+    get numberOfEventsOnOnePage() {
+        return this.numberOfEventsOnOnePage;
+    }
+        
+    set numberOfEventsOnOnePage(newValue) {
+        this.eventsOnOnePage = newValue;
+    }
+
 }
 
-
-export default EventService
+export default EventService;
