54297db6180c59a37b4b07416c434503d358081e
Roman-Lahoda <lagoda9515@gmail.com>
2021-09-14 22:10:03 +0300

    test

--- src/js/refs.js
+++
@@ -1,8 +0,0 @@
-const refs = {
-    searchForm: document.querySelector('.js-search-form'),
-    searchInput: document.querySelector('.js-search-input'),
-    eventsContainer: document.querySelector('.js-events-container'),
-    loadMore: document.querySelector('.js-load-more'),
-}
-
-export default refs;
