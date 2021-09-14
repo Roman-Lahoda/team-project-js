2ec66c3ce6a8a9a628c5d772de5f3369904ebde3
Roman-Lahoda <lagoda9515@gmail.com>
2021-09-14 22:18:25 +0300

    test/add-src

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    searchInput: document.querySelector('.js-search-input'),
+    eventsContainer: document.querySelector('.js-events-container'),
+    loadMore: document.querySelector('.js-load-more'),
+}
+
+export default refs;
