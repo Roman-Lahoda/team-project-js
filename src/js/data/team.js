c155b5b92c259e89cc6cf5ad06866b1150a28161
Roman-Lahoda <lagoda9515@gmail.com>
2021-09-17 21:15:14 +0300

    test-team

--- src/js/data/team.js
+++ src/js/data/team.js
@@ -1,23 +1,21 @@
-import img1 from '../../images/team/Roman_Lahoda.jpg'
-import img2 from '../../images/team/Anna_Prutnik.jpg'
-import img3 from '../../images/team/Dmitry_Dramaretsky.jpg'
-import img4 from '../../images/team/Natalia_Pavlova.jpg'
-import img5 from '../../images/team/Roman_Korotaev.jpg'
-import img6 from '../../images/team/Dina_Yermakova.jpg'
-import img7 from '../../images/team/Aleksey_Dibikov.jpg'
-import img8 from '../../images/team/Larisa_Shatil.jpg'
-import img9 from '../../images/team/Liudmyla_Kozachenko.jpg'
-import img10 from '../../images/team/Yevhenia_Mikhailova.jpg'
-
+import img1 from '../../images/team/Roman_Lahoda.jpg';
+import img2 from '../../images/team/Anna_Prutnik.jpg';
+import img3 from '../../images/team/Dmitry_Dramaretsky.jpg';
+import img4 from '../../images/team/Natalia_Pavlova.jpg';
+import img5 from '../../images/team/Roman_Korotaev.jpg';
+import img6 from '../../images/team/Dina_Yermakova.jpg';
+import img7 from '../../images/team/Aleksey_Dibikov.jpg';
+import img8 from '../../images/team/Larisa_Shatil.jpg';
+import img9 from '../../images/team/Liudmyla_Kozachenko.jpg';
+import img10 from '../../images/team/Yevhenia_Mikhailova.jpg';
 
 export default [
   {
     fullName: 'Roman Lahoda',
     position: 'Team Lead',
-    photo: `./${img1}`,
+    photo: `../images/team/Roman_Lahoda.jpg`,
     linkedIn: '#',
     gitHub: 'https://github.com/Roman-Lahoda/',
-    
   },
   {
     fullName: 'Anna Prutnik',
@@ -25,15 +23,13 @@ export default [
     photo: `./${img2}`,
     linkedIn: '#',
     gitHub: 'https://github.com/AnnaPrutnik',
-    
   },
   {
     fullName: 'Dmitry Dramaretsky',
     position: 'Developer',
     photo: `./${img3}`,
-    linkedIn:'#',
+    linkedIn: '#',
     gitHub: 'https://github.com/DmitryDr-dev',
-    
   },
   {
     fullName: 'Natalia Pavlova',
@@ -41,29 +37,27 @@ export default [
     photo: `./${img4}`,
     linkedIn: 'https://www.linkedin.com/in/natalia-pavlova/',
     gitHub: 'https://github.com/Lia-Pavlova',
-    
   },
   {
     fullName: 'Roman Korotaev',
     position: 'Developer',
     photo: `./${img5}`,
-    linkedIn:'#',
+    linkedIn: '#',
     gitHub: 'https://github.com/RomanKorotaev',
-    
   },
   {
     fullName: 'Dina Yermakova',
     position: 'Developer',
     photo: `./${img6}`,
     linkedIn: '#',
-    gitHub: 'https://github.com/YermakovaDina', 
+    gitHub: 'https://github.com/YermakovaDina',
   },
   {
     fullName: 'Aleksey Dibikov',
-    position: "Developer",
+    position: 'Developer',
     photo: `./${img7}`,
     linkedIn: 'https://www.linkedin.com/in/aleksey-dibikov-377a6921a/',
-    gitHub: 'https://github.com/Aleksey-Dibikov', 
+    gitHub: 'https://github.com/Aleksey-Dibikov',
   },
   {
     fullName: 'Larisa Shatil',
@@ -71,7 +65,6 @@ export default [
     photo: `./${img8}`,
     linkedIn: '#',
     gitHub: 'https://github.com/LarisaShatil',
-    
   },
   {
     fullName: 'Liudmyla Kozachenko',
@@ -79,7 +72,6 @@ export default [
     photo: `./${img9}`,
     linkedIn: '#',
     gitHub: 'https://github.com/Liucie',
-    
   },
   {
     fullName: 'Yevhenia Mikhailova',
