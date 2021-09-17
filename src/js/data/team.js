c4d0e261051de97c2803755a78d6d511e032f7e4
Roman-Lahoda <lagoda9515@gmail.com>
2021-09-17 21:20:00 +0300

    Update team.js

--- src/js/data/team.js
+++ src/js/data/team.js
@@ -13,7 +13,7 @@ export default [
   {
     fullName: 'Roman Lahoda',
     position: 'Team Lead',
-    photo: `../images/team/Roman_Lahoda.jpg`,
+    photo: `./images/team/Roman_Lahoda.jpg`,
     linkedIn: '#',
     gitHub: 'https://github.com/Roman-Lahoda/',
   },
